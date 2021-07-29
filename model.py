e3bcb01b72b2c0a7621ff69683ba8b415d3bdd67
frizwankhan <f.mohamedrizwan@gmail.com>
2021-07-30 00:07:29 +0530

    updated version

--- model.py
+++ model.py
@@ -13,21 +13,26 @@ class model_1(nn.Module):
         self.hidden_size = hidden_size
 
         self.embedding_mat = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_size, num_layers=num_layers, batch_first=True, bidirectional = True)
 
-        self.linear = nn.Linear(hidden_size, 1)
+        self.linear_1 = nn.Linear(hidden_size*2, 64)
+        self.linear_2 = nn.Linear(64,1)
+        self.drop = nn.Dropout(0.1)
         self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.embedding_mat(x)
         out, hidden = self.lstm(x)
         out = out[:, -1, :]
-        out = self.sigmoid(self.linear(out))
+        out = self.relu(self.linear_1(out))
+        out = self.drop(out)
+        out = self.sigmoid(self.linear_2(out))
 
         return out
 
 
 if __name__ == "__main__":
-    model = model_1(100, 23)
+    model = model_1(100, 25)
     x = np.random.randint(50, size=(10, 5))
     model(torch.tensor(x, dtype=torch.int))
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 from model import model_1
 import torch
@@ -6,6 +8,7 @@ import torch.nn as nn
 from dataset import CustomDataset
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
+import sklearn.metrics as metrics
 
 from tqdm import tqdm
 import pickle
@@ -28,16 +31,73 @@ class Train():
         total_correct = ((out > 0.5) == y).sum().detach().cpu().numpy()
         return total_correct / y.size(0)
 
+    def eval_metrics_plotting(self, train_epoch_loss, train_epoch_acc,
+                                    eval_epoch_loss, eval_epoch_acc,
+                                    y_true, y_pred,
+                                    y_true_train, y_pred_train):
+        fig1 = plt.figure(1)
+        plt.plot(list(range(len(train_epoch_loss))), train_epoch_loss, "b",  label="train_loss")
+        plt.plot(list(range(len(eval_epoch_loss))), eval_epoch_loss, "g", label="eval_loss")
+        plt.title("Train and Eval losses")
+        plt.legend()
+
+        fig2 = plt.figure(2)
+        plt.plot(list(range(len(train_epoch_acc))), train_epoch_acc, "b", label="train_acc")
+        plt.plot(list(range(len(eval_epoch_acc))), eval_epoch_acc, "g", label="eval_acc")
+        plt.title("Train and Eval Accuracy")
+        plt.legend()
+
+        fig3 = plt.figure(3)
+        fpr, tpr, threshold = metrics.roc_curve(y_true, y_pred)
+        roc_auc = metrics.auc(fpr, tpr)
+        plt.plot(fpr, tpr, label = "roc for evaluation")
+        print("threshold ROC for train:")
+        print(threshold)
+        fpr, tpr, threshold = metrics.roc_curve(y_true_train, y_pred_train)
+        roc_auc = metrics.auc(fpr, tpr)
+        plt.plot(fpr, tpr, label="roc for training")
+        print("threshold ROC for train:")
+        print(threshold)
+        plt.legend()
+        plt.title("roc curve")
+
+        fig4 = plt.figure(4)
+        precision, recall, threshold = metrics.precision_recall_curve(y_true, y_pred)
+        print("threshold PR for eval:")
+        print(threshold)
+        plt.plot(precision, recall, label="PR curve for eval")
+        precision, recall, threshold = metrics.precision_recall_curve(y_true_train, y_pred_train)
+        plt.plot(precision, recall, label="PR curve for training")
+        print("threshold PR for train:")
+        print(threshold)
+        plt.legend()
+        plt.title("PR curve")
+
+        print(f"fraction of positive examples i evalset is {sum(y_true)/len(y_true)}")
+        print(f"fraction of positive examples i trainset is {sum(y_true_train)/len(y_true_train)}")
+        print("Classification report for training set")
+        print(metrics.classification_report(y_true_train, y_pred_train>0.5))
+        print("Classification report for testing set")
+        print(metrics.classification_report(y_true, y_pred>0.5))
+
+        plt.show()
+
+
+
     def train_epoch(self, epoch):
         batch_losses = []
         batch_acc = []
+        preds, truth = [], []
         with tqdm(self.train_loader, unit="batch") as tbatch:
             for batch, (x, y) in enumerate(tbatch):
                 tbatch.set_description(f"Epoch {epoch+1}")
+                truth = truth + y.detach().tolist()
                 x = x.to(self.device)
                 y = y.to(self.device)
 
                 out = self.model(x).squeeze()
+
+                preds = preds + out.detach().cpu().tolist()
 
                 self.optimizer.zero_grad()
                 batch_loss = self.loss(out, y.type(torch.float))
@@ -50,24 +110,29 @@ class Train():
                 tbatch.set_postfix(batch_loss=batch_loss.detach().cpu().numpy(),
                                    batch_accuracy=acc)
 
-        return batch_losses, batch_acc
+        return batch_losses, batch_acc, preds, truth
 
     def eval_epoch(self):
         with torch.no_grad():
             batch_losses = []
             batch_acc = []
+            truth, preds = [], []
             for batch, (x, y) in enumerate(self.test_loader):
+                truth = truth + y.detach().tolist()
+
                 x = x.to(self.device)
                 y = y.to(self.device)
 
                 out = self.model(x)[0]
+                preds = preds + out.detach().cpu().tolist()
+
                 batch_loss = self.loss(out, y.type(torch.float))
 
                 acc = self.accuracy(out, y.type(torch.float))
                 batch_losses.append(batch_loss.detach().cpu().numpy())
                 batch_acc.append(acc)
 
-            return batch_losses, batch_acc
+            return batch_losses, batch_acc, preds, truth
 
     def start_training(self, epochs):
         train_epoch_loss = []
@@ -83,7 +148,7 @@ class Train():
         for epoch in range(epochs):
             ##### TRAINING #####
             self.model.train()
-            batch_losses, batch_acc = self.train_epoch(epoch)
+            batch_losses, batch_acc, train_preds, train_truth = self.train_epoch(epoch)
             print(f"epoch {epoch + 1} train loss is {sum(batch_losses) / len(batch_losses)}")
             print(f"epoch {epoch + 1} train acc is {sum(batch_acc) / len(batch_acc)}")
             print("all trainning losses, acc", end=" : ")
@@ -98,7 +163,7 @@ class Train():
 
             ##### EVALUATION #####
             self.model.eval()
-            batch_losses, batch_acc = self.eval_epoch()
+            batch_losses, batch_acc, eval_preds, eval_truth = self.eval_epoch()
             print(f"epoch {epoch + 1} eval loss is {sum(batch_losses) / len(batch_losses)}")
             print(f"epoch {epoch + 1} eval acc is {sum(batch_acc) / len(batch_acc)}")
             print("all eval losses, acc", end=" : ")
@@ -115,6 +180,8 @@ class Train():
         self.model.eval()
         torch.save(self.model.state_dict(), "model_1.pt")
 
+        return [train_epoch_loss, train_epoch_acc, eval_epoch_loss, eval_epoch_acc], [train_preds, train_truth, eval_preds, eval_truth]
+
 
 if __name__ == "__main__":
     data = pd.read_csv("airline_sentiment_analysis.csv")
@@ -128,4 +195,11 @@ if __name__ == "__main__":
     pickle.dump(train_dataset.vocab_to_i, pkl_file)
 
     obj = Train(train_dataset, test_dataset)
-    obj.start_training(20)
+    loss_acc, pred_truth = obj.start_training(12)
+
+    obj.eval_metrics_plotting(loss_acc[0], loss_acc[1],
+                              loss_acc[2], loss_acc[3],
+                              np.array(pred_truth[3]), np.array(pred_truth[2]),
+                              np.array(pred_truth[1]), np.array(pred_truth[0]))
+    print(len(pred_truth[0]), len(pred_truth[1]))
+
